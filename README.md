03b40554c6c25a677da8bf7d2af2710b83accf3e
cloveryima <cloverma1127@hotmail.com>
2020-10-29 18:12:50 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Coding Practice
+ Daily Practice
---
+++ array_index.py
@@ -0,0 +1,25 @@
+# integer
+array = [1,2,5,7,9,11,27,52,89,100]
+x = array.index(100)
+print(x)
+print(array)
+print(array[0])
+print(array[5])
+
+print(array[:3])
+print(array[2:6])
+
+print(array[:3:2]) # strt:end:step
+print(array[:7:3]) 
+print(array[1:9:4]) # doesn't include the end value
+print(array[:9]) # doesn't include the end value
+
+# string
+fruits = ["apple","banana","cherry","orange","grape"]
+y = fruits.index("cherry")
+print(y)
+
+
+
+
+
